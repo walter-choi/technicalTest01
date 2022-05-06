f64666fe04f3749c1b9a4677293c53674237ec1a
Walter Choi <walter.choi@6317.io>
2022-05-06 22:48:35 +0800

    update

--- device-api/test/start-unittest.sh
+++ device-api/test/start-unittest.sh
@@ -2,10 +2,28 @@
 echo "Date: " $(date)
 echo "-------------------------------------------"
 echo "Verifying prerequisites"
-verify_success=true
-which docker 1>/dev/null && echo "Docker: installed" || (echo "Docker: missed" && echo "please install docker and try again" && verify_success=false)
-[ $(node -v|cut -d . -f1|sed "s/v//g") -ge 10 ] && echo "Node Version: $(node -v)" || (echo "Node Version: $(node -v)" && echo "Please upgrade the node version to at least 10.0.0" && verify_success=false)
-[ ! ${verify_success} ] || exit 1
+# Verify docker installation
+which docker 1> /dev/null && docker_check=true || docker_check=false
+if [ ${docker_check} = true ]
+then
+    echo "Docker: installed"
+else
+    echo "Docker: missed"
+    echo "please install docker and try again"
+    verify_success=false
+fi
+# Verify node version greater than 14.0.0
+[ $(node -v|cut -d . -f1|sed "s/v//g") -ge 14 ] && node_version_check=true || node_version_check=false
+if [ ${node_version_check} = true ]
+then
+    echo "Node Version: $(node -v)" 
+else
+    echo "Node Version: $(node -v)"
+    echo "Please upgrade the node version to at least 14.0.0"
+    verify_success=false
+fi
+
+${verify_success:-true} && echo "Verification success" || exit 1
 echo "-------------------------------------------"
 echo "Installing dependency"
 npm install 
@@ -16,10 +34,10 @@ docker compose -f ../docker-compose.yml up --detach --always-recreate-deps && ec
 sleep 5
 echo "-------------------------------------------"
 echo "Starting unit test"
-mocha && (echo "Unit test success";unit_test_success=true) || (echo "Unit test fail";unit_test_success=false)
+mocha && echo "Unit test success" && unit_test_success=true || echo "Unit test fail"
 echo "-------------------------------------------"
 echo "Destroying test environment"
 docker compose down && echo "Test environment destoryed"
 echo "-------------------------------------------"
 echo "Result"
-${unit_test_success} && echo "Unit test success ,progress completed." || echo "Unit test fail, please check and run again"
+${unit_test_success:-false} && echo "Unit test success ,progress completed." || echo "Unit test fail, please check and run again"
